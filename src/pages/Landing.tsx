ceddea91f6dfe0edbb9eca2f38d3b0cbdae74d52
Abhigyan Khare <khareabhigyan2@gmail.com>
2026-07-24 01:02:39 +0530

    remove testiminoals

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,6 @@ import { Hero } from '@/components/landing/Hero'
 import { Features } from '@/components/landing/Features'
 import { Workflow } from '@/components/landing/Workflow'
 import { AgentArchitecture } from '@/components/landing/AgentArchitecture'
-import { Testimonials } from '@/components/landing/Testimonials'
 import { Footer } from '@/components/landing/Footer'
 
 export function Landing() {
@@ -14,7 +13,6 @@ export function Landing() {
       <Features />
       <Workflow />
       <AgentArchitecture />
-      <Testimonials />
       <Footer />
     </div>
   )
